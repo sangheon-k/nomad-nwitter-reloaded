a1d6417357ee7f7d9b233bafeca2ce90bc98700f
sangheon-k <sangheon.k@kakao.com>
2023-09-23 12:43:38 +0900

    feat : getAuth

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { useEffect, useState } from 'react';
 import LoadingScreen from './components/LoadingScreen';
+import { auth } from './firebase';
 
 const router = createBrowserRouter([
   {
@@ -38,7 +39,7 @@ const GlobalStyles = createGlobalStyle`
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
-    // wait for firebase
+    await auth.authStateReady();
     setLoading(false);
   };
   useEffect(() => {
