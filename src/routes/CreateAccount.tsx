616d1c1aca0d38de9f19b07b8bf03bda16e0b60b
sangheon-k <sangheon.k@kakao.com>
2023-09-28 22:18:04 +0900

    feat : createUserWithEmailAndPassword

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,7 +1,11 @@
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [userInfo, setUserInfo] = useState({
@@ -15,10 +19,21 @@ export default function CreateAccount() {
     setUserInfo({ ...userInfo, [name]: value });
   };
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // create an account
+      setLoading(true);
+      const { name, email, password } = userInfo;
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate('/');
     } catch (e) {
       // setError
     } finally {
@@ -28,7 +43,7 @@ export default function CreateAccount() {
 
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>Join 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -51,7 +66,10 @@ export default function CreateAccount() {
           type='password'
           required
         />
-        <Input type='submit' value='Create Account' />
+        <Input
+          type='submit'
+          value={isLoading ? 'Loading...' : 'Create Account'}
+        />
       </Form>
       {error !== '' ? <Error>{error}</Error> : null}
     </Wrapper>
