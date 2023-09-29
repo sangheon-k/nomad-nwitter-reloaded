0e4ccfddeec65ce083e75f9a1b5dbe030f11bc67
sangheon-k <sangheon.k@kakao.com>
2023-09-29 16:42:01 +0900

    feat : 네비게이션 바

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,9 @@
 import styled from 'styled-components';
-import { auth } from '../firebase';
 import PostTweetForm from '../components/PostTweetForm';
 
 export default function Home() {
-  const logout = () => {
-    auth.signOut();
-  };
-
   return (
     <Wrapper>
-      <button onClick={logout}>Log Out</button>
       <PostTweetForm />
     </Wrapper>
   );
