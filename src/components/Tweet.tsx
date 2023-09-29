638f5dbbb1d33fdf9edce6a7c19ca1653c58d38f
sangheon-k <sangheon.k@kakao.com>
2023-09-29 19:35:12 +0900

    feat : 트윗 삭제

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,12 +1,38 @@
 import styled from 'styled-components';
 import { ITweet } from './TimeLine';
+import { auth, db, storage } from '../firebase';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
 
-export default function Tweet({ username, photoUrl, tweet }: ITweet) {
+export default function Tweet({
+  username,
+  photoUrl,
+  tweet,
+  userId,
+  id,
+}: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm('Are you sure you want to delete this tweet?');
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, 'tweets', id));
+      if (photoUrl) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+        ) : null}
       </Column>
       <Column>{photoUrl ? <Photo src={photoUrl} /> : null}</Column>
     </Wrapper>
@@ -37,4 +63,16 @@ const Photo = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 15px;
+`;
+
+const DeleteButton = styled.button`
+  background-color: crimson;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
 `;
